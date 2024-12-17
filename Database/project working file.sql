226816aa385423afe36584eb08669f8998082605
TheLazyCactus <perraud.valentin@yahoo.fr>
2024-12-17 20:00:21 +0100

    update

---
+++ Database/project working file.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE sql_project;
+CREATE TABLE cardio (
+
+Entity VARCHAR(100),
+
+Code VARCHAR(5) ,
+Year INT,
+Age_death FLOAT
+id VARCHAR(50) PRIMARY KEY
+);
+
+DROP TABLE cardio;
+USE sql_project;
+SELECT * FROM cardio_death;
+SELECT * FROM obesity;
+SELECT * FROM meat;
